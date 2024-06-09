80bdf5a8c281ea7745728083373f67e0ce96ed97
Nobregaigor <igornobregadev@gmail.com>
2024-06-09 11:10:31 -0400

    Update read_materials.py

--- febio_python/xplt/xplt_parser/spec_3_0/utils/read_materials.py
+++ febio_python/xplt/xplt_parser/spec_3_0/utils/read_materials.py
@@ -1,23 +1,29 @@
-def read_materials(bf, TAGS, verbose=0):
-  from ...common.utils import search_block, check_block, read_bytes, console_log
-  # from numpy import zeros as npzeros
-  from collections import deque
-  
-  search_block(bf, TAGS, 'MATERIALS', verbose=verbose)
-  mat_names = deque()
-  mat_ids = deque()
+from ...common.utils import search_block, check_block, read_bytes, console_log
+from enum import IntEnum
+from collections import deque
 
-  while check_block(bf, TAGS, 'MATERIAL', verbose=verbose):
-    search_block(bf, TAGS, 'MATERIAL', verbose=verbose)
+def read_materials(bf, TAGS: IntEnum, verbose:int=0):
+    search_block(bf, TAGS, 'MATERIALS', verbose=verbose)
+    
+    mat_names = deque()
+    mat_ids = deque()
 
-    search_block(bf, TAGS, 'MAT_ID')
-    mat_ids.append(int(read_bytes(bf)))
+    while check_block(bf, TAGS, 'MATERIAL', verbose=verbose):
+        search_block(bf, TAGS, 'MATERIAL', verbose=verbose)
 
-    search_block(bf, TAGS, 'MAT_NAME', verbose=verbose)
-    mat_names.append(bf.read(64).decode('ascii').split('\x00')[0])
-  
-  console_log("---read_materials", 2, verbose)
-  console_log("->materials items: [mat_names, mat_ids]", 2, verbose)
-  console_log([mat_names, mat_ids], 2, verbose )
+        search_block(bf, TAGS, 'MAT_ID', verbose=verbose)
+        mat_ids.append(int(read_bytes(bf)))
 
-  return (mat_names, mat_ids)
+        search_block(bf, TAGS, 'MAT_NAME', verbose=verbose)
+        name_data = bf.read(64)
+        try:
+            mat_name = name_data.decode('ascii').split('\x00')[0]
+        except UnicodeDecodeError:
+            mat_name = name_data.decode('ascii', errors='replace').split('\x00')[0]
+        mat_names.append(mat_name)
+
+    console_log("---read_materials", 2, verbose)
+    console_log("->materials items: [mat_names, mat_ids]", 2, verbose)
+    console_log([mat_names, mat_ids], 2, verbose)
+
+    return (mat_names, mat_ids)
