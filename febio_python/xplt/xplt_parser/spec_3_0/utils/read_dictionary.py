f29fa32f9d2a7e833e654126a110acd0b93bef79
Nobregaigor <igornobregadev@gmail.com>
2024-06-09 11:10:17 -0400

    Update read_dictionary.py

--- febio_python/xplt/xplt_parser/spec_3_0/utils/read_dictionary.py
+++ febio_python/xplt/xplt_parser/spec_3_0/utils/read_dictionary.py
@@ -1,58 +1,55 @@
-def read_dictionary(bf, TAGS, verbose=0):
-  from ...common.utils import search_block, check_block, read_bytes, console_log
-  from collections import deque
+from ...common.utils import search_block, check_block, read_bytes, console_log
+from collections import deque
+from enum import IntEnum
 
-  # move cursor to "DICTIONARY"
-  search_block(bf, TAGS, 'DICTIONARY', verbose=verbose)
+def read_items(bf, TAGS, verbose):
+    item_types = deque()
+    item_formats = deque()
+    item_names = deque()
 
-  # move cursor to "DIC_NODAL"
-  search_block(bf, TAGS, 'DIC_NODAL', verbose=verbose)
+    while check_block(bf, TAGS, 'DIC_ITEM'):
+        search_block(bf, TAGS, 'DIC_ITEM', verbose=verbose)
 
-  item_types = deque()
-  item_formats = deque()  # 0: nodeal values, 1: elemental values
-  item_names = deque()
+        # Read item type
+        search_block(bf, TAGS, 'DIC_ITEM_TYPE', verbose=verbose)
+        item_types.append(int(read_bytes(bf)))
 
-  while check_block(bf, TAGS, 'DIC_ITEM'):
-    # get item type
-    search_block(bf, TAGS, 'DIC_ITEM', verbose=verbose)
-    search_block(bf, TAGS, 'DIC_ITEM_TYPE', verbose=verbose)
-    item_types.append(int(read_bytes(bf)))
-    
-    # get item format
-    search_block(bf, TAGS, 'DIC_ITEM_FMT', verbose=verbose)
-    item_formats.append(int(read_bytes(bf)))
+        # Read item format
+        search_block(bf, TAGS, 'DIC_ITEM_FMT', verbose=verbose)
+        item_formats.append(int(read_bytes(bf)))
 
-    # get item name
-    search_block(bf, TAGS, 'DIC_ITEM_NAME', verbose=verbose)
-    i_name = bf.read(64)
-    try:
-      item_names.append(i_name.decode('ascii').split('\x00')[0])
-    except UnicodeDecodeError:
-      item_names.append(i_name.split(b'\x00')[0].decode('ascii'))
+        # Read item name
+        search_block(bf, TAGS, 'DIC_ITEM_NAME', verbose=verbose)
+        i_name = bf.read(64)
+        try:
+            item_name = i_name.decode('ascii').split('\x00')[0]
+        except UnicodeDecodeError:
+            item_name = i_name.split(b'\x00')[0].decode('ascii')
+        item_names.append(item_name)
 
-  # Explore Dictionary domain
-  # move cursor to "DIC_DOMAIN"
-  search_block(bf, TAGS, 'DIC_DOMAIN', verbose=verbose)
+    return item_types, item_formats, item_names
 
-  while check_block(bf, TAGS, 'DIC_ITEM'):
-    search_block(bf, TAGS, 'DIC_ITEM', verbose=verbose)
-    search_block(bf, TAGS, 'DIC_ITEM_TYPE', verbose=verbose)
-    item_types.append(int(read_bytes(bf)))
+def read_dictionary(bf, TAGS: IntEnum, verbose:int = 0):
+    # Navigate to "DICTIONARY" and "DIC_NODAL"
+    search_block(bf, TAGS, 'DICTIONARY', verbose=verbose)
+    search_block(bf, TAGS, 'DIC_NODAL', verbose=verbose)
 
-    search_block(bf, TAGS, 'DIC_ITEM_FMT', verbose=verbose)
-    item_formats.append(int(read_bytes(bf)))
+    # Read nodal items
+    item_types, item_formats, item_names = read_items(bf, TAGS, verbose)
 
-    search_block(bf, TAGS, 'DIC_ITEM_NAME', verbose=verbose)
-    i_name = bf.read(64)
-    try:
-      item_names.append(i_name.decode('ascii').split('\x00')[0])
-    except UnicodeDecodeError:
-      item_names.append(i_name.split(b'\x00')[0].decode('ascii'))
-  
-  console_log("---read_dictionary:", 2,verbose)
-  console_log("->dict items: [item_types, item_formats, item_names]", 2, verbose)
-  console_log([item_types, item_formats, item_names], 2, verbose)
+    # Navigate to "DIC_DOMAIN" and read domain items
+    search_block(bf, TAGS, 'DIC_DOMAIN', verbose=verbose)
+    domain_types, domain_formats, domain_names = read_items(bf, TAGS, verbose)
 
-  states_dict = {"types": item_types, "formats": item_formats, "names": item_names}
-  
-  return states_dict
+    # Append domain items to nodal items
+    item_types.extend(domain_types)
+    item_formats.extend(domain_formats)
+    item_names.extend(domain_names)
+
+    console_log("---read_dictionary:", 2, verbose)
+    console_log("->dict items: [item_types, item_formats, item_names]", 2, verbose)
+    console_log([item_types, item_formats, item_names], 2, verbose)
+
+    states_dict = {"types": item_types, "formats": item_formats, "names": item_names}
+
+    return states_dict
