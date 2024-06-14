c34033e1c749357908632d325fdd0863f323bbb5
Nobregaigor <igornobregadev@gmail.com>
2024-06-14 16:33:59 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,4 +53,6 @@ nbsphinx_allow_errors = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = []
+
